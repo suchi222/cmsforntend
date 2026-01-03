6b7d41d22cabaf2bb7788ac2d9b67c2df0427d33
suchi <thapasuchitra10@gmail.com>
2026-01-03 21:30:40 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+const App = () => {
+  return (
+   <BrowserRouter>
+   <Routes>
+   <Route path="/" element={<Home/>}/>
+   <Route path="/about" element={<About/>}/>
+   <Route path="/contact" element={<Contact/>}/>
+   </Routes>
+   </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import './Home.css';
+const Home = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod esse
+        nesciunt magnam tenetur harum eligendi, omnis ipsa voluptatibus itaque
+        reprehenderit maiores cupiditate odit quo doloremque illum sed
+        consectetur, accusamus cum nam voluptates, est possimus nostrum soluta
+        eos? Ut amet quam ipsam doloremque itaque obcaecati, excepturi mollitia
+        sint eveniet dolores non accusamus repellat esse distinctio impedit ipsa
+        optio! Molestiae rem nulla consequuntur debitis optio, ab dolorum
+        suscipit nam corporis necessitatibus ea earum beatae facere minima iusto
+        maiores commodi repellendus dignissimos incidunt sunt explicabo
+        laudantium praesentium voluptate. Beatae esse sed atque, tempora,
+        ratione eveniet aspernatur, a perferendis impedit eum nulla quas
+        doloremque.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
